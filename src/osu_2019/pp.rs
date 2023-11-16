fbc9e3c3ec1673047840c0c86f8fe34e71d9fd0e
tsunyoku <tsunyoku@gmail.com>
2023-11-16 15:02:54 +0000

    attempt

--- src/osu_2019/pp.rs
+++ src/osu_2019/pp.rs
@@ -268,7 +268,7 @@ impl<'m> OsuPP<'m> {
 
         if streams_nerf < 1.09 {
             let acc_factor = (1.0 - self.acc.unwrap()).abs();
-            acc_depression = (0.84 - acc_factor).max(0.5);
+            acc_depression = (0.87 - acc_factor).max(0.5);
 
             if acc_depression > 0.0 {
                 aim_value *= acc_depression;
